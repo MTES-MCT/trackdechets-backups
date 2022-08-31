921fb22dae617588496401ff50b9d949a30bbbe6
benoitguigal <benoit.guigal@gmail.com>
2022-08-31 09:42:32 +0200

    parse env variable

--- src/s3.ts
+++ src/s3.ts
@@ -1,21 +1,30 @@
 import S3 from "aws-sdk/clients/s3";
 import { Stream } from "stream";
 
+const {
+  S3_ENDPOINT,
+  S3_REGION,
+  S3_ACCESS_KEY_ID,
+  S3_SECRET_ACCESS_KEY,
+  S3_BUCKET,
+  S3_ENDPOINT_PART_SIZE
+} = process.env;
+
 const s3 = new S3({
-  endpoint: process.env.S3_ENDPOINT,
-  region: process.env.S3_REGION,
-  accessKeyId: process.env.S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  signatureVersion: "v4",
+  endpoint: S3_ENDPOINT,
+  region: S3_REGION,
+  accessKeyId: S3_ACCESS_KEY_ID,
+  secretAccessKey: S3_SECRET_ACCESS_KEY,
+  signatureVersion: "v4"
 });
 
 export function s3Writer(filename: string) {
   const pass = new Stream.PassThrough();
   const params = {
-    Bucket: process.env.S3_BUCKET,
+    Bucket: S3_BUCKET,
     Key: filename,
     Body: pass,
-    partSize: process.env.S3_ENDPOINT_PART_SIZE,
+    partSize: S3_ENDPOINT_PART_SIZE ? parseInt(S3_ENDPOINT_PART_SIZE, 10) : 500
   };
   const upload = s3.upload(params).promise();
   return { writer: pass, upload };
